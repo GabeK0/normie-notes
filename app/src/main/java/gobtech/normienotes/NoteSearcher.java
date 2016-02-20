4a4c86c526b3aa277f285d6ea94913d4fa95c290
Gabriel <gabek0001@gmail.com>
2016-02-19 16:53:15 -0800

    Hopefully volley will work now

--- app/src/main/java/gobtech/normienotes/NoteSearcher.java
+++ app/src/main/java/gobtech/normienotes/NoteSearcher.java
@@ -6,7 +6,7 @@ import android.widget.ListView;
 
 
 
-import java.util.ArrayList;
+import com.android.volley.Request;import com.android.volley.RequestQueue;import com.android.volley.Response;import com.android.volley.VolleyError;import com.android.volley.toolbox.StringRequest;import com.android.volley.toolbox.Volley;import java.util.ArrayList;
 
 /**
  * Created by gabrielk on 2/18/16.
