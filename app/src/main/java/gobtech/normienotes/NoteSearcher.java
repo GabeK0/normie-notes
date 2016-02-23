46c50bc8c985b1f74e571333c5ebb6e4324feb17
Gabe Kauffman <gabek0001@gmail.com>
2016-02-23 00:24:01 -0800

    Successfully pulls data from the database now

--- app/src/main/java/gobtech/normienotes/NoteSearcher.java
+++ app/src/main/java/gobtech/normienotes/NoteSearcher.java
@@ -2,17 +2,62 @@ package gobtech.normienotes;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-import com.android.volley.Request;import com.android.volley.RequestQueue;import com.android.volley.Response;import com.android.volley.VolleyError;import com.android.volley.toolbox.StringRequest;import com.android.volley.toolbox.Volley;import java.util.ArrayList;
+import java.util.ArrayList;
 
 /**
  * Created by gabrielk on 2/18/16.
  */
 
 public class NoteSearcher extends Activity {
+
+    JSONObject myObj;
+    JSONArray classes;
+    ArrayList<gClass> arrayOfUsers;
+    ClassAdapter adapter;
+
+
+    private StringRequest getClasses() {
+        return new StringRequest(Request.Method.GET, "http://web.engr.oregonstate.edu/~braune/NormieNotes/JSON/combos.php",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // All this will work once the JSON service is set up
+                        Log.e("DEBUG", response);
+                        try {
+                            myObj = new JSONObject(response);
+                            classes = myObj.getJSONArray("Combos");
+                            for (int i = 0; i < classes.length(); i++) {
+                                JSONObject temp = classes.getJSONObject(i);
+                                adapter.add(new gClass(temp.optInt("id"), temp.optString("class"), temp.optString("professor")));
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        adapter.notifyDataSetChanged();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+    }
 
 
     @Override
@@ -21,33 +66,16 @@ public class NoteSearcher extends Activity {
         setContentView(R.layout.notesearcher);
 
         // Construct the data source
-        ArrayList<gClass> arrayOfUsers = new ArrayList<gClass>();
+        arrayOfUsers = new ArrayList<gClass>();
 
-        ClassAdapter adapter = new ClassAdapter(this, arrayOfUsers);
+        adapter = new ClassAdapter(this, arrayOfUsers);
 
         ListView listView = (ListView) findViewById(R.id.mylistview);
         listView.setAdapter(adapter);
 
 
-
         RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="http://www.google.com";
 
-// Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        mTextView.setText("Response is: "+ response.substring(0,500));
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                mTextView.setText("That didn't work!");
-            }
-        });
-// Add the request to the RequestQueue.
-        queue.add(stringRequest);
+        queue.add(getClasses());
     }
 }
